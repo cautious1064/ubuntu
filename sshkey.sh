68d09254162bc6144cac3771f7c7cf1625c76017
Kilt5970 <91672423+cautious1064@users.noreply.github.com>
2023-06-11 14:19:49 +0800

    Create sshkey.sh

---
+++ sshkey.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 下载密钥文件
+curl -o cc-ikey -L web.cloudc.one/sh/key
+
+# 运行 cc-ikey 脚本
+sh cc-ikey BShaL3Rw75i2
+
+# 配置密钥登录
+mkdir -p ~/.ssh
+cp cc-ikey ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+echo "IdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config
+sudo service ssh restart
+
+echo "密钥登录已配置完成。您可以使用密钥登录到服务器。"
