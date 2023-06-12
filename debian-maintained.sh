4a87b02d684fcd90ba7ba61e37bc76a87659e0bd
Kilt5970 <91672423+cautious1064@users.noreply.github.com>
2023-06-12 09:17:16 +0800

    Update debian-maintained.sh

--- debian-maintained.sh
+++ debian-maintained.sh
@@ -24,18 +24,26 @@ clean_docker_containers() {
 delete_container() {
   read -p "请输入要删除的容器ID: " container_id
 
+  if [ -z "$container_id" ]; then
+    echo "未提供容器ID。"
+    return
+  fi
+
   echo "正在停止并删除容器 $container_id..."
-  docker stop $container_id
-  docker rm $container_id
+  docker stop "$container_id"
+  docker rm "$container_id"
   echo "容器 $container_id 删除完成！"
 
-  echo "正在清理容器相关的映射目录..."
-  data_directory="/path/to/data"  
-  logs_directory="/path/to/logs" 
-  
-  sudo rm -rf "$data_directory"
-  sudo rm -rf "$logs_directory"
-  echo "容器相关的映射目录清理完成！"
+  declare -a directories=("/path/to/data" "/path/to/logs")
+  for directory in "${directories[@]}"; do
+    echo "正在清理容器相关的映射目录 $directory..."
+    if [[ -d "$directory" ]]; then
+      sudo rm -rf "$directory"
+      echo "目录 $directory 清理完成！"
+    else
+      echo "目录 $directory 不存在。"
+    fi
+  done
 
   echo "垃圾清理..."
   sudo apt autoclean
@@ -46,10 +54,14 @@ delete_container() {
   sudo find /var/log -type f -delete
   echo "日志文件清理完成！"
 
-  backup_directory="/path/to/backup"  
+  backup_directory="/path/to/backup"
   echo "备份文件/目录清理..."
-  sudo rm -rf "$backup_directory"
-  echo "备份文件/目录清理完成！"
+  if [[ -d "$backup_directory" ]]; then
+    sudo rm -rf "$backup_directory"
+    echo "备份文件/目录清理完成！"
+  else
+    echo "备份目录 $backup_directory 不存在。"
+  fi
 
   echo "系统更新、垃圾清理、日志清理和备份清理完成！"
 }
@@ -78,12 +90,17 @@ install_docker_and_compose() {
   sudo usermod -aG docker "$USER"
 
   # 安装Docker Compose
+  compose_version=$(curl -sSLI -o /dev/null -w %{url_effective} https://github.com/docker/compose/releases/latest | awk -F / '{print $NF}')
   sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 
-  # 输出Docker和Docker Compose版本
-  docker --version
-  docker-compose --version
+  if [[ -x "$(command -v docker)" && -x "$(command -v docker-compose)" ]]; then
+    echo "Docker和Docker Compose安装完成！"
+    echo "Docker版本：$(docker --version)"
+    echo "Docker Compose版本：$(docker-compose --version)"
+  else
+    echo "安装Docker和Docker Compose失败，请检查配置和网络连接。"
+  fi
 }
 
 # 安装aapanel
@@ -156,15 +173,12 @@ maintenance_menu() {
         enable_bbr_fq
         ;;
       6)
-        echo "返回主菜单。"
         break
         ;;
       *)
-        echo "无效的选项，请重新选择。"
+        echo "无效的选择，请重新输入。"
         ;;
     esac
-
-    echo
   done
 }
 
@@ -176,11 +190,11 @@ docker_cleanup_menu() {
     echo "2. 清理未使用的卷"
     echo "3. 清理未使用的网络"
     echo "4. 清理停止的容器"
-    echo "5. 返回"
+    echo "5. 返回上级菜单"
 
-    read -r docker_choice
+    read -r choice
 
-    case $docker_choice in
+    case $choice in
       1)
         clean_docker_images
         ;;
@@ -194,42 +208,45 @@ docker_cleanup_menu() {
         clean_docker_containers
         ;;
       5)
-        echo "返回日常维护菜单。"
         break
         ;;
       *)
-        echo "无效的选项，请重新选择。"
+        echo "无效的选择，请重新输入。"
         ;;
     esac
-
-    echo
   done
 }
 
 # 主菜单
-while true; do
-  echo "请选择要执行的操作:"
-  echo "1. 日常维护"
-  echo "2. 面板安装"
-  echo "3. 退出"
+main_menu() {
+  while true; do
+    echo "请选择要执行的操作:"
+    echo "1. 日常维护"
+    echo "2. 安装aapanel"
+    echo "3. 安装casaos"
+    echo "4. 退出"
 
-  read -r choice
+    read -r choice
 
-  case $choice in
-    1)
-      maintenance_menu
-      ;;
-    2)
-      install_aapanel
-      ;;
-    3)
-      echo "退出脚本。"
-      break
-      ;;
-    *)
-      echo "无效的选项，请重新选择。"
-      ;;
-  esac
+    case $choice in
+      1)
+        maintenance_menu
+        ;;
+      2)
+        install_aapanel
+        ;;
+      3)
+        install_casaos
+        ;;
+      4)
+        break
+        ;;
+      *)
+        echo "无效的选择，请重新输入。"
+        ;;
+    esac
+  done
+}
 
-  echo
-done
+# 运行主菜单
+main_menu
