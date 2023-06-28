f59fa8896a739a5bed176dc795595daead980164
Kilt5970 <91672423+cautious1064@users.noreply.github.com>
2023-06-28 09:58:09 +0800

    Update sshkey.sh

--- sshkey.sh
+++ sshkey.sh
@@ -12,10 +12,16 @@ cp cc-ikey ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 echo "IdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config
 
-# 允许root用户登录SSH
+# 允许 root 用户登录 SSH
 sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 
-# 重启SSH服务
+# 重启 SSH 服务
 sudo service ssh restart
 
 echo "密钥登录已配置完成。您可以使用密钥登录到服务器。"
+
+# 添加功能保持客户端连接 6 小时
+echo "ClientAliveInterval 360" | sudo tee -a /etc/ssh/sshd_config
+echo "ClientAliveCountMax 36" | sudo tee -a /etc/ssh/sshd_config
+sudo service ssh restart
+echo "已添加功能：客户端连接将保持活动状态 6 小时。"
