98e31bd4e5f0d26adbcd873f736c0545987ce773
Kilt5970 <91672423+cautious1064@users.noreply.github.com>
2023-06-29 16:20:26 +0800

    Update debian-maintained.sh

--- debian-maintained.sh
+++ debian-maintained.sh
@@ -83,7 +83,7 @@ update_and_cleanup_system() {
   echo "正在更新软件包和基础工具..."
   apt update -y
   apt upgrade -y
-  sudo apt install curl sudo neofetch vim -y
+  apt install curl sudo neofetch vim -y
   echo "软件包更新完成！"
 
   echo "垃圾清理..."
