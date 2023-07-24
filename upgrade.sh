5da9a0bfb545c9b164a71ebb8e7a3356e2d7faef
Kilt5970 <91672423+cautious1064@users.noreply.github.com>
2023-07-24 08:46:05 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
 
-# 检查当前用户是否为 root 用户
-if [[ $EUID -ne 0 ]]; then
-   echo "请以 root 用户身份运行此脚本。"
-   exit 1
+# 检查是否以 root 权限运行脚本
+if [ "$EUID" -ne 0 ]; then
+  echo "请使用 root 权限运行此脚本"
+  exit 1
 fi
 
-# 检测系统类型
-if [ -f /etc/debian_version ]; then
-   # Debian 系统
-   apt update
-   apt upgrade -y
-   apt dist-upgrade -y
-   apt autoremove -y
-   apt autoclean
-elif [ -f /etc/lsb-release ]; then
-   # Ubuntu 系统
-   apt update
-   apt upgrade -y
-   do-release-upgrade -f DistUpgradeViewNonInteractive
+# 检查是否为 Ubuntu 系统
+if [ ! -f "/etc/os-release" ]; then
+  echo "无法找到 /etc/os-release 文件，此脚本仅支持 Ubuntu 系统"
+  exit 1
+fi
+
+# 获取当前 Ubuntu 版本信息
+source /etc/os-release
+
+echo "当前运行的 Ubuntu 版本为: $PRETTY_NAME"
+read -p "是否要继续升级到新版本？(y/n): " choice
+
+if [ "$choice" != "y" ] && [ "$choice" != "Y" ]; then
+  echo "取消升级操作"
+  exit 0
+fi
+
+# 更新已安装的软件包和系统组件
+apt update
+apt upgrade -y
+
+# 安装升级工具
+apt install -y update-manager-core
+
+# 开始升级 Ubuntu 版本
+do-release-upgrade
+
+# 检查升级是否成功，可以根据实际情况进行处理
+if [ $? -eq 0 ]; then
+  echo "Ubuntu 版本升级完成！"
 else
-   echo "无法确定系统类型。"
-   exit 1
+  echo "Ubuntu 版本升级过程中发生错误，请检查日志以及终端输出，以便解决问题。"
 fi
-
-# 重启系统
-shutdown -r now
