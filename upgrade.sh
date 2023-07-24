404af9068deb205ba79d6486abbdef105fbec5ff
Kilt5970 <91672423+cautious1064@users.noreply.github.com>
2023-07-24 09:25:21 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,60 +1,19 @@
 #!/bin/bash
 
-# 检查是否以 root 权限运行脚本
-if [ "$EUID" -ne 0 ]; then
-  echo "请使用 root 权限运行此脚本"
-  exit 1
-fi
+# 提示用户备份数据
+echo "请在执行升级前备份重要数据！"
+read -p "按回车键继续升级，或按 Ctrl+C 取消："
 
-# 检查是否为 Ubuntu 系统
-if [ ! -f "/etc/os-release" ]; then
-  echo "无法找到 /etc/os-release 文件，此脚本仅支持 Ubuntu 系统"
-  exit 1
-fi
+# 更新软件包列表
+sudo apt update
 
-# 获取当前 Ubuntu 版本信息
-source /etc/os-release
+# 更新已安装的软件包
+sudo apt upgrade -y
 
-echo "当前运行的 Ubuntu 版本为: $PRETTY_NAME"
+# 执行系统升级
+sudo do-release-upgrade
 
-# 检查是否为 LTS 版本
-if [[ "$UBUNTU_CODENAME" == "bionic" ]]; then
-  echo "当前运行的 Ubuntu 版本为 LTS 版本"
-else
-  echo "当前运行的 Ubuntu 版本不是 LTS 版本，将修改为 LTS 版本"
-  # 将当前版本修改为 LTS 版本
-  sed -i 's/Prompt=normal/Prompt=lts/' /etc/update-manager/release-upgrades
-fi
-
-# 定义升级函数
-perform_upgrade() {
-  # 更新软件源并升级已安装的软件包
-  apt update
-  apt upgrade -y
-
-  # 安装升级工具
-  apt install -y update-manager-core
-
-  # 开始升级 Ubuntu 版本，并将错误日志记录到文件
-  do-release-upgrade 2> /tmp/upgrade_error.log
-
-  # 检查升级是否成功
-  if [ $? -eq 0 ]; then
-    echo "Ubuntu 版本升级完成！"
-  else
-    echo "Ubuntu 版本升级过程中发生错误，请检查日志以便解决问题。"
-    cat /tmp/upgrade_error.log  # 输出错误日志内容
-  fi
-
-  # 删除临时错误日志文件
-  rm /tmp/upgrade_error.log
-}
-
-read -p "是否要升级到新的版本？(y/n): " choice
-
-# 检查用户输入是否为合法选项
-if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
-  perform_upgrade
-else
-  echo "取消升级操作"
-fi
+# 如果升级完成后需要重启，提示用户重启
+echo "系统升级已完成！"
+read -p "按回车键重启计算机，或按 Ctrl+C 取消重启："
+sudo reboot
