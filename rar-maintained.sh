af5cbdf56b3caabf7af36ca0507bc0596981c15b
Kilt5970 <91672423+cautious1064@users.noreply.github.com>
2023-10-03 17:09:36 +0800

    Update rar-maintained.sh

--- rar-maintained.sh
+++ rar-maintained.sh
@@ -1,37 +1,34 @@
 #!/bin/bash
 
-echo "RAR压缩和解压工具"
+# 检查rar是否已经安装，如果没有安装就安装rar
+if ! command -v rar &> /dev/null
+then
+    echo "安装rar..."
+    sudo apt-get update
+    sudo apt-get install rar
+else
+    echo "rar已经安装."
+fi
 
-select choice in "压缩文件/文件夹" "解压RAR文件" "退出"; do
-    case $choice in
-        "压缩文件/文件夹")
-            read -p "请输入要压缩的文件或文件夹路径: " filepath
-            if [ ! -e "$filepath" ]; then
-                echo "路径不存在"
-                exit 1
-            fi
+# 输入要压缩的文件或文件夹路径
+read -p "请输入要压缩的文件或文件夹路径: " source_path
 
-            output_filename=$(basename "$filepath").rar
-            rar a "$output_filename" "$filepath"
-            echo "压缩完成"
-            ;;
-        "解压RAR文件")
-            read -p "请输入要解压的RAR文件路径: " filepath
-            if [ ! -e "$filepath" ]; then
-                echo "路径不存在"
-                exit 1
-            fi
+# 检查路径是否存在
+if [ ! -e "$source_path" ]
+then
+    echo "路径不存在，请重新输入."
+    exit 1
+fi
 
-            # 提取目标路径
-            target_dir=$(dirname "$filepath")
-            unrar x "$filepath" "$target_dir"
-            echo "解压完成"
-            ;;
-        "退出")
-            break
-            ;;
-        *) 
-            echo "无效选项"
-            ;;
-    esac
-done
+# 输入压缩密码
+read -s -p "请输入压缩密码: " password
+echo
+
+# 输入目标路径
+read -p "请输入目标路径: " target_path
+
+# 创建RAR压缩文件
+echo "正在创建RAR压缩文件..."
+rar a -p"$password" -v5g "$target_path/compressed.rar" "$source_path"
+
+echo "压缩完成。"
