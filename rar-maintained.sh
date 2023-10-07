73b06ac716413dd5d4a881e7e7c452977ae55c8d
Kilt5970 <91672423+cautious1064@users.noreply.github.com>
2023-10-07 11:15:53 +0800

    Update rar-maintained.sh

--- rar-maintained.sh
+++ rar-maintained.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 
-# 检查rar是否已经安装，如果没有安装就安装rar
-if ! command -v rar &> /dev/null; then
-    echo "安装rar..."
-    sudo apt-get update
-    sudo apt-get install rar
-    if [ $? -ne 0 ]; then
-        echo "安装rar失败，请手动安装rar后再运行此脚本。"
-        exit 1
-    fi
-else
-    echo "rar已经安装."
-fi
-
 # 菜单函数
 show_menu() {
     echo "请选择一个操作:"
-    echo "1. 压缩文件或文件夹"
-    echo "2. 解压缩文件"
+    echo "1. 压缩文件或文件夹为ZIP"
+    echo "2. 解压缩ZIP文件"
     echo "3. 退出"
 }
 
@@ -32,19 +19,12 @@ compress_files() {
         return
     fi
 
-    # 输入压缩密码
-    read -p "请输入压缩密码: " password
-
-    # 自动计算分卷大小，默认单位为GB
-    read -p "请输入分卷大小（默认单位为GB，直接回车使用默认值）: " volume_size
-    volume_size=${volume_size:-5}  # 默认值为5GB
-
     # 获取源文件或文件夹的基本名称
     source_basename=$(basename "$source_path")
 
-    # 创建RAR压缩文件
-    echo "正在创建RAR压缩文件..."
-    rar a -p"$password" -v${volume_size}g "$source_basename.rar" "$source_path"
+    # 创建UTF-8编码的ZIP压缩文件
+    echo "正在创建UTF-8编码的ZIP压缩文件..."
+    zip -r -O UTF-8 "$source_basename.zip" "$source_path"
 
     if [ $? -eq 0 ]; then
         echo "压缩完成。"
@@ -55,21 +35,18 @@ compress_files() {
 
 # 解压缩函数
 extract_files() {
-    # 输入要解压的RAR文件路径
-    read -p "请输入要解压的RAR文件路径: " rar_path
+    # 输入要解压的ZIP文件路径
+    read -p "请输入要解压的ZIP文件路径: " zip_path
 
-    # 检查RAR文件是否存在
-    if [ ! -e "$rar_path" ]; then
-        echo "RAR文件不存在，请重新输入."
+    # 检查ZIP文件是否存在
+    if [ ! -e "$zip_path" ]; then
+        echo "ZIP文件不存在，请重新输入."
         return
     fi
 
-    # 输入解压密码
-    read -p "请输入解压密码: " password
-
-    # 解压RAR文件
-    echo "正在解压RAR文件..."
-    unrar x -p"$password" "$rar_path"
+    # 解压ZIP文件
+    echo "正在解压ZIP文件..."
+    unzip "$zip_path"
 
     if [ $? -eq 0 ]; then
         echo "解压完成。"
