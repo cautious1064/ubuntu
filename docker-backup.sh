ccfb406202aed55b985c78c872a094643227bb7c
Kilt5970 <91672423+cautious1064@users.noreply.github.com>
2023-11-17 10:37:18 +0800

    Update docker-backup.sh

--- docker-backup.sh
+++ docker-backup.sh
@@ -10,42 +10,45 @@ echo "5. 备份 Docker 数据卷"
 echo "6. 恢复 Docker 数据卷"
 read -p "输入选项 (1/2/3/4/5/6): " option
 
+# 询问用户输入备份/恢复路径
+read -p "请输入备份/恢复的路径: " path
+
 # 备份Docker容器和镜像
 backup_containers_images() {
-  docker save -o /root/backup/containers.tar $(docker ps -aq)
-  docker save -o /root/backup/images.tar $(docker images -q)
+  docker ps -aq | jq -r '.[]' | xargs docker save -o "$path/containers.tar"
+  docker images -q | jq -r '.[]' | xargs docker save -o "$path/images.tar"
   echo "容器和镜像备份完成。"
 }
 
 # 恢复Docker容器和镜像
 restore_containers_images() {
-  docker load -i /root/restore/containers.tar
-  docker load -i /root/restore/images.tar
+  docker load -i "$path/containers.tar"
+  docker load -i "$path/images.tar"
   echo "容器和镜像恢复完成。"
 }
 
 # 备份Docker网络设置
 backup_networks() {
-  docker network save -o /root/backup/networks.tar $(docker network ls -q)
+  docker network ls -q | jq -r '.[]' | xargs docker network save -o "$path/networks.tar"
   echo "网络设置备份完成。"
 }
 
 # 恢复Docker网络设置
 restore_networks() {
   docker network create -d bridge my_network
-  docker network import /root/restore/networks.tar
+  docker network import "$path/networks.tar"
   echo "网络设置恢复完成。"
 }
 
 # 备份Docker数据卷
 backup_volumes() {
-  docker run --rm -v my_volume:/backup_data -v /root/backup:/backup ubuntu tar czf /backup/volume_backup.tar.gz /backup_data
+  docker run --rm -v my_volume:/backup_data -v "$path:/backup" ubuntu tar czf /backup/volume_backup.tar.gz /backup_data
   echo "数据卷备份完成。"
 }
 
 # 恢复Docker数据卷
 restore_volumes() {
-  docker run --rm -v my_volume:/restore_data -v /root/restore:/restore ubuntu tar xzf /restore/volume_backup.tar.gz -C /restore_data
+  docker run --rm -v my_volume:/restore_data -v "$path:/restore" ubuntu tar xzf /restore/volume_backup.tar.gz -C /restore_data
   echo "数据卷恢复完成。"
 }
 
